94d033ecf51c1122750dc9365471eb05e8ed886d
Cedric Le Sergent <87324244+clsdjo30@users.noreply.github.com>
2022-10-08 17:13:16 +0200

    create subsidiaryPermission bis

---
+++ src/Entity/SubsidiaryPermission.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SubsidiaryPermissionRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SubsidiaryPermissionRepository::class)]
+class SubsidiaryPermission
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'subsidiaryPermissions')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?subsidiary $subsidiary = null;
+
+    #[ORM\ManyToOne(inversedBy: 'subsidiaryPermissions')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Permission $permission = null;
+
+    #[ORM\Column]
+    private ?bool $isActive = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSubsidiary(): ?subsidiary
+    {
+        return $this->subsidiary;
+    }
+
+    public function setSubsidiary(?subsidiary $subsidiary): self
+    {
+        $this->subsidiary = $subsidiary;
+
+        return $this;
+    }
+
+    public function getPermission(): ?Permission
+    {
+        return $this->permission;
+    }
+
+    public function setPermission(?Permission $permission): self
+    {
+        $this->permission = $permission;
+
+        return $this;
+    }
+
+    public function isIsActive(): ?bool
+    {
+        return $this->isActive;
+    }
+
+    public function setIsActive(bool $isActive): self
+    {
+        $this->isActive = $isActive;
+
+        return $this;
+    }
+}
